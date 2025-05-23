38a5434f0d3a6923b36808c27b78165dccbde9b9
JaxonC-06 <jaxoncostain@outlook.com>
2025-05-22 21:48:11 -0600

    Added files for styling, updated layout on frontend, added client side js for add another option.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 // Client facing scripts here
+$(document).ready(function() {
+  $("#add-option").on('click', function() {
+    const $option = $(`
+      <span class="individual-option">
+        <input type="text">
+        <i class="fa-regular fa-message"></i>
+        <i class="fa-regular fa-trash-can"></i>
+      </span>
+    `);
+
+    $('.options').append($option);
+  });
+});
