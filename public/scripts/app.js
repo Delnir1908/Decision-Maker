b7720dcc78c06c1c5bdea3488681e2c1a3ef6d6e
JaxonC-06 <jaxoncostain@outlook.com>
2025-05-24 14:45:08 -0600

    Succesfully update delete button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,38 @@
 // Client facing scripts here
+$(document).ready(function() {
+  $("#add-option").on('click', function() {
+    const $option = $(`
+      <div class="options">
+        <span class="individual-option">
+          <input type="text">
+          <i class="fa-regular fa-message custom-message"></i>
+          <i class="fa-regular fa-trash-can"></i>
+        </span>
+      </div>
+    `);
+
+    $(this).before($option);
+  });
+
+  $(document).on('click', '.custom-message', function() {
+    const $option = $(this).closest('.individual-option');
+    const $desc = $option.next('.description');
+
+    if ($desc.length) {
+      $desc.toggle();
+    } else {
+      const $description = $(`
+        <span class="description">
+          <textarea name="description" type="text" class="message" placeholder="Add an optional description here"></textarea>
+        </span>
+      `);
+
+      $option.after($description);
+    }
+  });
+
+  $(document).on('click', '.fa-trash-can', function() {
+    const $option = $(this).closest('.options');
+    $option.remove();
+  })
+});
