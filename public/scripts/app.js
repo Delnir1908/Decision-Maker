9474579b63eb004048a2d54fba5329730aed900f
JaxonC-06 <jaxoncostain@outlook.com>
2025-05-23 18:13:42 -0600

    Fixed description button, it now toggles the visibility of a single description.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     const $option = $(`
       <span class="individual-option">
         <input type="text">
-        <i class="fa-regular fa-message"></i>
+        <i class="fa-regular fa-message custom-message"></i>
         <i class="fa-regular fa-trash-can"></i>
       </span>
     `);
@@ -12,13 +12,20 @@ $(document).ready(function() {
     $('.options').append($option);
   });
 
-  $(document).on('click', '.fa-message', function() {
-    const $description = $(`
-      <span>
-        <textarea type="text" class="message" placeholder="Add an optional description here"></textarea>
-      </span>
-    `);
+  $(document).on('click', '.custom-message', function() {
+    const $option = $(this).closest('.individual-option');
+    const $desc = $option.next('.description');
 
-    $(this).parent('.individual-option').after($description);
+    if ($desc.length) {
+      $desc.toggle();
+    } else {
+      const $description = $(`
+        <span class="description">
+          <textarea name="description" type="text" class="message" placeholder="Add an optional description here"></textarea>
+        </span>
+      `);
+
+      $option.after($description);
+    }
   });
 });
